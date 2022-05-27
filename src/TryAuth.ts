ae87f55ff4c0b419c6d53b3e36f6a93d1ff529fa
Alex Tochetto <atochetto@sysphera.com>
2022-05-27 16:17:25 -0300

    authorization check id_token nonce

--- src/TryAuth.ts
+++ src/TryAuth.ts
@@ -1,3 +1,10 @@
+import JwtDecode from 'jwt-decode';
+
+class TryAuthAuthorizationResponse {
+    AccessToken: string = null;
+    IdToken: string = null;
+}
+
 class TryAuthAuthorizationOptions {
     ClientId: string = null;
     IssuerEndpoint: string = null;
@@ -5,39 +12,120 @@ class TryAuthAuthorizationOptions {
     Scopes: string = null;
 }
 
+class TryAuthError {
+    error: string = null;
+}
+
+interface JwtPayload {
+    iss?: string;
+    sub?: string;
+    aud?: string[] | string;
+    nonce?: string;
+    exp?: number;
+    nbf?: number;
+    iat?: number;
+}
+
 export default class TryAuth {
-    constructor() { }
+    private readonly NONCE_KEY = 'tryauth_authorization_nonce_key'
+    private readonly RESPONSE_TYPE_IDTOKEN_TOKEN: string = 'id_token token';
+    private readonly RESPONSE_TYPE_TOKEN_IDTOKEN: string = 'token id_token';
+    private readonly RESPONSE_TYPE_CODE: string = 'code';
+    private readonly ACCESS_TOKEN: string = 'access_token';
+    private readonly ID_TOKEN: string = 'id_token';
+    constructor(public localStorage: LocalStorageBackend = new LocalStorage()) { }
 
     public async Authorize(tryAuthAuthorizationOptions: TryAuthAuthorizationOptions): Promise<void> {
+        if (tryAuthAuthorizationOptions.ResponseType === this.RESPONSE_TYPE_IDTOKEN_TOKEN || tryAuthAuthorizationOptions.ResponseType === this.RESPONSE_TYPE_TOKEN_IDTOKEN) {
+            await this.AuthorizeIdTokenToken(tryAuthAuthorizationOptions);
+        }
+        else if (tryAuthAuthorizationOptions.ResponseType === this.RESPONSE_TYPE_CODE) {
+
+        }
+        else {
+            // throw error
+        }
+    }
+
+    public async CheckAuthorize(): Promise<TryAuthAuthorizationResponse | TryAuthError> {
+        const tryAuthAuthorizationResponse: TryAuthAuthorizationResponse = this.GetResponseData();
+        const nonceValid = await this.ValidateIdTokenNonce(tryAuthAuthorizationResponse.IdToken);
+        if (!nonceValid) {
+            let tryAuthError: TryAuthError = new TryAuthError();
+            tryAuthError.error = 'invalid nonce';
+            return tryAuthError;
+        }
+        // validate expired iat
+        // validate not before nbf
+        this.localStorage.removeItem(this.NONCE_KEY);
+        return tryAuthAuthorizationResponse;
+    }
+
+    private GetResponseData(): TryAuthAuthorizationResponse {
+        let map = this.GetAuthorizationKeyValue();
+        const access_token = map.get(this.ACCESS_TOKEN);
+        const id_token = map.get(this.ID_TOKEN);
+        let tryAuthAuthorizationResponse: TryAuthAuthorizationResponse = new TryAuthAuthorizationResponse();
+        tryAuthAuthorizationResponse.AccessToken = access_token;
+        tryAuthAuthorizationResponse.IdToken = id_token;
+        return tryAuthAuthorizationResponse;
+    }
+
+    private async ValidateIdTokenNonce(idToken: string): Promise<boolean> {
+        let jwtPayload = JwtDecode<JwtPayload>(idToken);
+        const nonceStored = await this.localStorage.getItem(this.NONCE_KEY);
+        if (jwtPayload.nonce === nonceStored) {
+            return true;
+        }
+        return false;
+    }
+
+    private GetAuthorizationKeyValue(): Map<string, string> {
+        let hash: string = window.location.hash; // this will receive the '#id_token=...'
+        if (hash.startsWith('#')) {
+            // throw error
+        }
+        hash = hash.substring(1); // remove the '#'
+        const split: string[] = hash.split('&');
+        let map = new Map<string, string>();
+        for (let i = 0; i < split.length; i++) {
+            const keys = split[i];
+            const keyValue = keys.split('=');
+            map.set(keyValue[0], keyValue[1]);
+        }
+        return map;
+    }
+
+    private async AuthorizeIdTokenToken(tryAuthAuthorizationOptions: TryAuthAuthorizationOptions): Promise<void> {
         const headers = this.SetDefaultHeaders();
-        let authorizeEndpoint = this.GetAuthorizeEndpoint(tryAuthAuthorizationOptions.IssuerEndpoint);
-        authorizeEndpoint = authorizeEndpoint + '?client_id=' + tryAuthAuthorizationOptions.ClientId;
-        authorizeEndpoint = authorizeEndpoint + '&redirect_uri=' + encodeURIComponent(window.location.origin);
-        authorizeEndpoint = authorizeEndpoint + '&response_type=' + tryAuthAuthorizationOptions.ResponseType;
-        authorizeEndpoint = authorizeEndpoint + '&scope=' + tryAuthAuthorizationOptions.Scopes;
-        authorizeEndpoint = authorizeEndpoint + '&nonce=3f6d2b3421cddf48e6b70ed7f2bb4a4be7EWZ7qSK';
-        return new Promise(
-            (resolve, reject) => {
-                const xhr: XMLHttpRequest = new XMLHttpRequest();
-                xhr.withCredentials = false; // add Authorize in header
-                xhr.onload = () => {
-                    window.location.href = xhr.responseURL;
-                };
-                xhr.open('GET', authorizeEndpoint, true);
-                for (let i: number = 0; i < headers.length; i++) {
-                    const header: [string, string] = headers[i];
-                    xhr.setRequestHeader(header[0], this.EnsureASCII(header[1]));
-                }
-                //xhr.send(JSON.stringify(request));
-                xhr.send(null);
+        const authorizeEndpoint = await this.GetAuthorizeEndpoint(tryAuthAuthorizationOptions);
+        return new Promise<void>((resolve, reject) => {
+            const xhr: XMLHttpRequest = new XMLHttpRequest();
+            xhr.withCredentials = false; // add Authorize in header
+            xhr.onload = () => {
+                window.location.href = xhr.responseURL; // redirect to response location header of authorize endpoint
+            };
+            xhr.open('GET', authorizeEndpoint, true); // call the authorize endpoint
+            for (let i: number = 0; i < headers.length; i++) {
+                const header: [string, string] = headers[i];
+                xhr.setRequestHeader(header[0], this.EnsureASCII(header[1]));
             }
-        );
+            //xhr.send(JSON.stringify(request));
+            xhr.send(null);
+        });
     }
 
     private SetDefaultHeaders(): [string, string][] {
         let headers: [string, string][] = [];
         headers.push(['Content-Type', 'application/x-www-form-urlencoded']); // together withCredentials do not as for CORS
         return headers;
+    }
+
+    private async SetNonceLocalStorage(): Promise<string> {
+        const crypto = new Crypto(30);
+        const nonce: string = crypto.Create();
+        await this.localStorage.setItem(this.NONCE_KEY, nonce);
+        return nonce;
     }
 
     private GetHeaderValue(headers: [string, string][], name: string): string {
@@ -49,12 +137,23 @@ export default class TryAuth {
         return null;
     }
 
-    private GetTokenEndpoint(issuerEndpoint: string): string {
+    private GetConnectAuthorizeEndpoint(issuerEndpoint: string): string {
+        return issuerEndpoint + '/connect/authorize';
+    }
+
+    private GetConnectTokenEndpoint(issuerEndpoint: string): string {
         return issuerEndpoint + '/connect/token';
     }
 
-    private GetAuthorizeEndpoint(issuerEndpoint: string): string {
-        return issuerEndpoint + '/connect/authorize';
+    private async GetAuthorizeEndpoint(tryAuthAuthorizationOptions: TryAuthAuthorizationOptions): Promise<string> {
+        const nonce: string = await this.SetNonceLocalStorage();
+        let authorizeEndpoint = this.GetConnectAuthorizeEndpoint(tryAuthAuthorizationOptions.IssuerEndpoint);
+        authorizeEndpoint = authorizeEndpoint + '?client_id=' + tryAuthAuthorizationOptions.ClientId;
+        authorizeEndpoint = authorizeEndpoint + '&redirect_uri=' + encodeURIComponent(window.location.origin);
+        authorizeEndpoint = authorizeEndpoint + '&response_type=' + tryAuthAuthorizationOptions.ResponseType;
+        authorizeEndpoint = authorizeEndpoint + '&scope=' + tryAuthAuthorizationOptions.Scopes;
+        authorizeEndpoint = authorizeEndpoint + '&nonce=' + nonce;
+        return authorizeEndpoint;
     }
 
     private EnsureASCII(data: string): string {
@@ -81,5 +180,86 @@ export default class TryAuth {
             encoded += '\\u' + index.toString(16).toUpperCase();
         }
         return (encoded);
+    }
+}
+
+class Crypto {
+    private _size: number = null;
+    private _charset: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    constructor(size: number) {
+        this._size = size;
+    }
+    public Create(): string {
+        const buffer = new Uint8Array(this._size);
+        for (let i = 0; i < this._size; i += 1) {
+            buffer[i] = (Math.random() * this._charset.length) | 0;
+        }
+        return this.BufferToString(buffer);
+    }
+
+    BufferToString(buffer: Uint8Array): string {
+        let state = [];
+        for (let i = 0; i < buffer.byteLength; i += 1) {
+            let index = buffer[i] % this._charset.length;
+            state.push(this._charset[index]);
+        }
+        return state.join('');
+    }
+}
+
+interface ILocalStorage {
+    readonly length: number;
+    clear(): void;
+    getItem(key: string): string | null;
+    removeItem(key: string): void;
+    setItem(key: string, data: string): void;
+}
+
+abstract class LocalStorageBackend {
+    //When passed a key `name`, will return that key's value.
+    public abstract getItem(name: string): Promise<string | null>;
+    //When passed a key `name`, will remove that key from the storage.
+    public abstract removeItem(name: string): Promise<void>;
+    //When invoked, will empty all keys out of the storage.
+    public abstract clear(): Promise<void>;
+    //The setItem() method of the `ILocalStorage` interface,
+    //when passed a key name and value, will add that key to the storage,
+    //or update that key's value if it already exists.
+    public abstract setItem(name: string, value: string): Promise<void>;
+}
+
+class LocalStorage extends LocalStorageBackend {
+    private _storage: ILocalStorage;
+    constructor(storage?: ILocalStorage) {
+        super();
+        this._storage = storage || window.localStorage;
+    }
+    public getItem(name: string): Promise<string | null> {
+        return new Promise<string | null>((resolve, reject) => {
+            const value = this._storage.getItem(name);
+            if (value) {
+                resolve(value);
+            } else {
+                resolve(null);
+            }
+        });
+    }
+    public removeItem(name: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this._storage.removeItem(name);
+            resolve();
+        });
+    }
+    public clear(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this._storage.clear();
+            resolve();
+        });
+    }
+    public setItem(name: string, value: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this._storage.setItem(name, value);
+            resolve();
+        });
     }
 }
